186f951fd25bd28dbb905f78589590864ed99a1f
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-20 10:41:24 -0500

    change the location to ornaldo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-function init(){
-	//alert('it works');
-	var el = document.getElementById('map');
-	var myLoc = new google.maps.LatLng(28.552493075299854, -81.35807994280118);
-    var mapOptions = {
-    	center: myLoc,
-    	zoom: 18,
-    	mapTypeId: google.maps.MapTypeId.SATELLITE,
-    	mapTypeControlOptions:{
-    		position: google.maps.ControlPosition.BOTTOM_CENTER
-    	}
-    };
+function initMap() {
+// The location of Uluru
+	const orlando = { lat: 28.532750418214206, lng: -81.38378947662801, };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 11,
+  center: orlando,
+});
+// The marker, positioned at Uluru
+const marker = new google.maps.Marker({
+  position: orlando,
+  map: map,
+});
 }
-  
-
 
 google.maps.event.addDomListener(window, 'load', init);
