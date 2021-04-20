fe45a0540683e601f3534459c65f2e6fa06e0a30
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-19 21:29:19 -0500

    trying to make the js code run

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function init(){
 	//alert('it works');
+	var map;
 	map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    zoom: 11,
   });
   
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+//google.maps.event.addDomListener(window, 'load', init);
