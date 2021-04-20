bc21d35a8b0452d40c916bf04aa151914d0609ce
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-19 21:15:57 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 function init(){
-	//alert('it works');
-	map = new google.maps.Map(document.getElementById("map"), {
+	alert('it works');
+	/*map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+  */
 }
 
 google.maps.event.addDomListener(window, 'load', init);
