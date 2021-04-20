2d6f3d743be06202c72605047ccd03971a2b00b5
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-20 11:21:41 -0500

    add a marker called disney, with a disney picture. add event listeners to have a window of content pop up when hovered over the picture and closed when hovered off .

--- js/script.js
+++ js/script.js
@@ -5,21 +5,39 @@ function initMap() {
 	const map = new google.maps.Map(document.getElementById("map"), {
 	  zoom: 11,
 	  center: orlando,
-	  mapTypeId: google.maps.MapTypeId.SATELLITE,
-	  mapTypeControlOptions: {
-	  		position: google.maps.ControlPosition.BOTTOM_CENTER
-	  }
 	});
 
+	
 	const marker = new google.maps.Marker({
 	  position: orlando,
 	  map: map,
 	});
 
+	//scale image
+	var icon = {
+		url: "images/disney.jpeg",
+		scaledSize: new google.maps.Size(100,50),
+	};
+	//create disney location 
 	const disney = {lat:28.384559737896573, lng: -81.56459180702294};
 	const markertwo = new google.maps.Marker({
+		position: disney,
+		map: map,
+		animation: google.maps.Animation.DROP,
+		icon: icon
+	});
 
-	})
+	var disneyString = '<h2> Walt Disney World Resort</h2><p>Well-known attraction comprising large resorts, dining, shops & 4 theme parks with rides & shows.</p>' 
+	var windowinfo = new google.maps.InfoWindow({
+		content: disneyString
+	});
+
+	google.maps.event.addListener(markertwo, 'mouseover',function(){
+		windowinfo.open(map, markertwo);
+	});
+	google.maps.event.addListener(markertwo, 'mouseout', function(){
+		windowinfo.close(map,markertwo);
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', init);
