cedb6ad4e79d281b7fa4809a449a52f4e30f9e59
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-20 10:28:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function init(){
-	//alert('it works');
+	alert('it works');
 	var el = document.getElementById('map');
 	var myLoc = new google.maps.LatLng(28.552493075299854, -81.35807994280118);
     var mapOptions = {
