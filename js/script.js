c6169cbf581f61f3320f47522ce00f50ee789045
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-20 10:27:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 function init(){
 	//alert('it works');
-	var map;
-	map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 11,
-  });
-  
+	var el = document.getElementById('map');
+	var myLoc = new google.maps.LatLng(28.552493075299854, -81.35807994280118);
+    var mapOptions = {
+    	center: myLoc,
+    	zoom: 18,
+    	mapTypeId: google.maps.MapTypeId.SATELLITE,
+    	mapTypeControlOptions:{
+    		position: google.maps.ControlPosition.BOTTOM_CENTER
+    	}
+    };
 }
+  
+
 
 google.maps.event.addDomListener(window, 'load', init);
