ad4ee2c076cfeecf8d3f626c5825410ae4c19054
olemeha <77707992+olemeha@users.noreply.github.com>
2021-04-20 11:04:51 -0500

    add image and create a new marker

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 function initMap() {
-// The location of Uluru
-	const orlando = { lat: 28.532750418214206, lng: -81.38378947662801, };
-// The map, centered at Uluru
-const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 11,
-  center: orlando,
-});
-// The marker, positioned at Uluru
-const marker = new google.maps.Marker({
-  position: orlando,
-  map: map,
-});
+	//location of orlando
+	const orlando = { lat: 28.532750418214206, lng: -81.38378947662801};
+	//zoomed in onto the city 
+	const map = new google.maps.Map(document.getElementById("map"), {
+	  zoom: 11,
+	  center: orlando,
+	  mapTypeId: google.maps.MapTypeId.SATELLITE,
+	  mapTypeControlOptions: {
+	  		position: google.maps.ControlPosition.BOTTOM_CENTER
+	  }
+	});
+
+	const marker = new google.maps.Marker({
+	  position: orlando,
+	  map: map,
+	});
+
+	const disney = {lat:28.384559737896573, lng: -81.56459180702294};
+	const markertwo = new google.maps.Marker({
+
+	})
 }
 
 google.maps.event.addDomListener(window, 'load', init);
